49e8dc4c087d94b876d4b1e871b80b1a608c12e7
Sawaira <sawairasharif21@gmail.com>
2023-04-15 22:22:53 +0500

    Js Assignment Task # 2

--- scripts/Assignment.js
+++ scripts/Assignment.js
@@ -11,4 +11,41 @@ message to display the calculated zakat value. The message should look like this
 var zakatPercentage = 0.025;
 var userInput = +prompt("Enter your wealth amount.");
 var result = userInput * zakatPercentage;
-alert("Your zakat value is ",result);
+alert("Your zakat value is " + result);
+
+// Task 2:
+/*calculate the fitrah amount, first, ask the user to enter the total number of family
+members using the prompt and store the value in a variable called
+"familyMembers". Next, ask the user to choose a fitrah method by providing them
+options using the prompt, and store the selected method and its price in variables.
+Then, use an if-else block to check the user's input and calculate the fitrah amount
+by multiplying the selected method's price with the number of family members.
+Finally, display the calculated fitrah amount using an alert message.*/
+
+var familyMembers = +prompt("For Fitrah please enter the total number of your family members");
+var method = +prompt("For Gandum type '1', Jau type '2', Khajoor type '3', Khishmish type '4', Ajwa Khajoor type '5'");
+// for Gandum
+if(method == 1){
+    alert("Your family's total fitra for Gandum is "+(familyMembers*320));
+}
+//for Jau
+else if(method == 2){
+    alert("Your family's total fitra for Jau is "+(familyMembers*800));
+}
+//for Khajoor
+else if(method == 3){
+    alert("Your family's total fitra for Khajoor is "+(familyMembers*2800));
+}
+// for kishmish
+else if(method == 4){
+    alert("Your family's total fitra for Khismish is "+(familyMembers*6400));
+}
+// for Ajwa Khajoor
+else if(method == 5){
+    alert("Your family's total fitra for Ajwa Khajoor is "+(familyMembers*10400));
+}
+// invalid input
+else{
+    alert("Invalid Input Try again!")
+}
+
