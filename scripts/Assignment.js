a8e7467446681ba00c295119bdb0fc4e0e99acf4
Sawaira <sawairasharif21@gmail.com>
2023-04-16 15:29:17 +0500

    Js Assignment Task # 7

--- scripts/Assignment.js
+++ scripts/Assignment.js
@@ -144,5 +144,45 @@ else{
     alert("Invalid input");
 }
 
+// Task 7:
+/*Create a program that asks the user for their nationality, gender, and age using the prompt
+function. The program should then use nested if-else statements to determine if the
+person is eligible to vote.
+First, the program should check if the person is Pakistani or Indian. If they are not, the
+program should display a message saying they are not eligible to vote.
+If they are Pakistani or Indian, the program should then check their gender. If the person is
+male and over the age of 18, they are eligible to vote. If the person is female and over the
+age of 18, the program should ask if they are married. If they are married, they are eligible
+to vote. If they are not married, they are not eligible to vote.
+If the person is under 18, the program should display a message saying they are not eligible
+to vote.*/
+
+var inputNation = prompt("Enter your nationality:");
+
+if (inputNation == 'Pakistan' || inputNation == 'Pakistani' || inputNation == 'Pakistan' || inputNation == 'pakistani' ||inputNation == 'pakistan' || inputNation == 'PAKISTANI' || inputNation == 'PAKISTAN' || inputNation == 'Indian' || inputNation == 'India' || inputNation == 'indian' || inputNation == 'india' ){
+    alert("You are eligible to vote! click 'Ok' to proceed further:")
+    var inputGender = prompt("Please enter your gender Male or Female:");
+    var inputAge = +prompt("Please enter your age:");
+    if (inputGender == 'male'||'men'||'Male'||'Men'||'MALE'||'MEN' && inputAge >= 19){
+        alert("You are eligible to vote!");
+    }
+    else if(inputGender == 'female'||'Female'||'FeMale'||'feMale'||'FEMALE' && inputAge >= 19){
+        var maritalStatus = prompt("Are you married? Yes or No");
+        if(maritalStatus == 'Yes'||'yes'||'YES'){
+            alert("You are eligible to vote");
+        }
+        else{
+            alert("You are not eligible to vote because of unmarried status")
+        }
+
+    }
+    else{
+        alert("You are not eligible to vote because your age should be over 18")
+    }
+
+}
+else{
+    alert("For eligibility your nationality must b Pakistani or Indian")
+}
 
 
